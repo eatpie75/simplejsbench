65c8c80af12336478523abcd4c6effe501f7546c
eatpie75 <eatpie75@gmail.com>
2015-06-30 16:24:59 -0700

    auto generate slug make fields required

--- models/test.js
+++ models/test.js
@@ -1,12 +1,20 @@
 var mongoose = require('mongoose');
+var slug = require('slug');
 
 var TestSchema = new mongoose.Schema({
-  'name':String,
-  'slug':String,
-  'creator':String,
+  'name':{'type':String, 'required':true},
+  'slug':{'type':String, 'required':true},
+  'creator':{'type':String, 'required':true},
   'date_created':{'type':Date, 'default':Date.now},
   'versions':[],
-  'test':String,
+  'test':{'type':String, 'required':true},
+});
+
+TestSchema.pre('validate', function(next) {
+  if (this.slug === undefined) {
+    this.slug = slug(this.name);
+  }
+  next();
 });
 
 TestSchema.methods.get_url = function() {
