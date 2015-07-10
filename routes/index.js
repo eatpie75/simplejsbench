535ccd1a3ff2f671ea5ef05369931003d14975ff
eatpie75 <eatpie75@gmail.com>
2015-07-09 19:38:11 -0700

    linted

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   req.models.Test.find(function(err, tests) {
     if (err) {console.error(err);}
 
